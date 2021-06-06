8fb7633245fc29918313b61dd77ca4c368aeb53e
VLADDUCH <ducorp@ymail.com>
2021-06-06 13:16:06 -0400

    Edit draft 1 with comments

--- PyBank/main.ipynb/draft.py
+++ PyBank/main.ipynb/draft.py
@@ -16,28 +16,28 @@ GDP = "Greatest Decrease in Profits"
 #len (monthly_records - nomonthly_records)
 
 #The net total amount of Profit/Losses over the entire period.
-ie profit= inser data set 
-total = ()
-x= sum (total)
+#ie profit= inser data set 
+#total = ()
+#x= sum (total)
 
-The average of the changes in Profit/Losses over the entire period.
-import statictics 
-print(statistics.mean([]))
+#The average of the changes in Profit/Losses over the entire period.
+#import statictics 
+#print(statistics.mean([]))
 
-The greatest increase in profits (date and amount) over the entire period.
-GIP= max ()
-print GIP
+#The greatest increase in profits (date and amount) over the entire period.
+#GIP= max ()
+#print GIP
 
-The greatest decrease in losses (date and amount) over the entire period.
-GDP = min ()
-print GDP
+#The greatest decrease in losses (date and amount) over the entire period.
+#GDP = min ()
+#print GDP
 
-Your resulting analysis should look similar to the following:
-Financial Analysis
-----------------------------
+#Your resulting analysis should look similar to the following:
+#Financial Analysis
+#----------------------------
 print ("Total Months: " + TM)
 print ("Total: " + T)
 print ("Average Change: " + AC)
 print ("Greatest Increase in Profits: " + GIP)
 print ("Greatest Decrease in Profits: " + GDP)
-Your final script should print the analysis to the terminal and export a text file with the results.
+#Your final script should print the analysis to the terminal and export a text file with the results.
